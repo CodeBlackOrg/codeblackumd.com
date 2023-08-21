becf5df22bf43d5234d918b9683a5ef003a7d8aa
BeimnetA <beimnetak@gmail.com>
2023-08-21 18:14:16 -0400

    Started SponsorForm

---
+++ app/about/Components/EboardMembers.js
@@ -0,0 +1,72 @@
+export const members = [
+    {
+        img: "https://static.wixstatic.com/media/a310c9_fa26dfcfc2ad4bcf99b743a73aaee523~mv2.jpg/v1/crop/x_0,y_230,w_1222,h_900/fill/w_912,h_672,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/IMG_9951_edited.jpg", 
+        role: "Pres",
+        name: "name",
+        desc: "test",
+        link: "test"
+    },
+    {
+        img: "", 
+        role: "",
+        name: "",
+        desc: "",
+        link: ""
+    },
+    {
+        img: "", 
+        role: "",
+        name: "",
+        desc: "",
+        link: ""
+    },
+    {
+        img: "", 
+        role: "",
+        name: "",
+        desc: "",
+        link: ""
+    },
+    {
+        img: "", 
+        role: "",
+        name: "",
+        desc: "",
+        link: ""
+    },
+    {
+        img: "", 
+        role: "",
+        name: "",
+        desc: "",
+        link: ""
+    },
+    {
+        img: "", 
+        role: "",
+        name: "",
+        desc: "",
+        link: ""
+    },
+    {
+        img: "", 
+        role: "",
+        name: "",
+        desc: "",
+        link: ""
+    },
+    {
+        img: "", 
+        role: "",
+        name: "",
+        desc: "",
+        link: ""
+    },
+    {
+        img: "", 
+        role: "",
+        name: "",
+        desc: "",
+        link: ""
+    },
+]
