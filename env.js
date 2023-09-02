27a140c1d96a2d4a70bc09bd12fcdee73f43666a
Nathan T <nathantemesgen92@gmail.com>
2023-09-02 19:05:33 -0400

    worked on Calendar

--- env.js
+++ env.js
@@ -6,13 +6,20 @@ import { getFirestore } from "firebase/firestore";
 require('dotenv').config()
 
 export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MEASURMENTID,
+  // apiKey: process.env.REACT_APP_API_KEY,
+  // authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  // projectId: process.env.REACT_APP_PROJECTID,
+  // storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  // messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  // appId: process.env.REACT_APP_APPID,
+  // measurementId: process.env.REACT_APP_MEASURMENTID,
+  apiKey: "AIzaSyCqWuiUgPlpaXRLRKDcqJuCoawqnO-9hC0",
+  authDomain: "codeblack-a757b.firebaseapp.com",
+  projectId: "codeblack-a757b",
+  storageBucket: "codeblack-a757b.appspot.com",
+  messagingSenderId: "603771662146",
+  appId: "1:603771662146:web:9403d6680ddc9eec8322db",
+  measurementId: "G-F3Z6B9C0FS",
 };
 
 const app = initializeApp(firebaseConfig);
